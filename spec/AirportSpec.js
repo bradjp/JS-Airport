66e978f44b9a3e96c14a59e9eb217d6b5120a879
Brad Phillips <bjrp@protonmail.com>
2019-12-18 15:40:27 +0000

    Passes third unit test

---
+++ spec/AirportSpec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Airport', function(){
+  var airport;
+  var plane;
+  beforeEach(function(){
+    airport = new Airport();
+    plane = jasmine.createSpy('plane',['land']);
+  });
+  it('begins with no planes', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+  it('can sanction landing', function(){
+    airport.landingPermitted(plane);
+    expect(airport.planes()).toEqual([plane]);
+  });
+});
